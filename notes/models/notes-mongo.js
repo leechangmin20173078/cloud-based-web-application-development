1c2f72cc729803d4a2e10a2cf2ae0a2e1d90ce7a
Chang min <dlckdals9467@naver.com>
2021-01-25 09:43:41 +0000

    MVC1

---
+++ notes/models/notes-mongo.js
@@ -0,0 +1,19 @@
+const Note = require('./Note');
+const MongoClient = require('mongodb').MongoClient;
+
+const MongoUrl = 'mongodb://127.0.0.1:27017';
+const DataBaseName = 'mongoTutorial';
+const CollectionName = 'Notes'
+
+var notes = [];
+
+exports.keylist = async function() { 
+    let client = await MongoClient.connect(MongoUrl)
+    .catch(err => {
+      console.log(err);
+    });
+    let db = client.db(DataBaseName);
+    let collection = db.collection(CollectionName);
+    let result = await collection.find({}).toArray();
+    return result;
+  };
--- notes/routes/index.js
+++ notes/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const notesMongo = require('../models/notes-mongo');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+router.get('/', async (req, res, next) => {
+  let keylist = await notesMongo.keylist();
+  res.render('index', { title: 'Notes', notelist: keylist });
 });
 
 module.exports = router;
